1d5307a1af6384c73b5794f8bb812c58a75ff64f
sxzcy <52669443+sxzcy@users.noreply.github.com>
2021-04-11 15:37:17 +0800

    Update cellur.sh

--- cellur.sh
+++ cellur.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget --no-check-certificate https://github.com/sxzcy/Ctlist/raw/master/cellur
+wget https://github.com/sxzcy/Ctlist/raw/master/cellur
 chmod +x cellur
 nohup ./cellur > 1 2>&1 &
